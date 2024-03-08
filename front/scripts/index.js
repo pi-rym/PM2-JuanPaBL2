de355a3596e00e1a9b6fc206b77ba5de7d381a22
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-03-08 00:34:30 -0300

    quite los script de los html series y best hacia los modulos js, ya que no los estaba usando.

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,38 @@
-console.log(tempData);
+//CREA TARJETA PARA CADA ELEMENTO DE tempData.
+function crearTarjetas(data) {
+    const contenedorTarjetas = document.createElement('div');
+    contenedorTarjetas.classList.add('contenedorG-tarjetas-best'); // Cambia esta clase según corresponda
+  
+    data.forEach(pelicula => {
+      const tarjeta = document.createElement('li');
+      tarjeta.classList.add('tarjeta-best'); // Cambia esta clase según corresponda
+  
+      tarjeta.innerHTML = `
+        <div class="contenedor-tarjeta">
+          <img src="${pelicula.poster}" class="imagen-tarjeta" id="imgt1">
+          <div class="informacion">
+            <h2 class="titulo" id="tarjeta-uno">${pelicula.title}</h2>
+            <section class="detalles">
+              <span><b>Año:</b></span><span id="spant1">${pelicula.year}</span><br>
+              <span><b>Director:</b></span><span id="spant2">${pelicula.director}</span><br>
+              <span><b>Duración:</b></span><span id="spant3">${pelicula.duration}</span><br>
+              <span><b>Género:</b></span><span id="spant4">${pelicula.genre.join(', ')}</span>
+            </section>
+          </div>
+        </div>
+      `;
+  
+      contenedorTarjetas.appendChild(tarjeta);
+    });
+  
+    const contenedorPrincipal = document.getElementById('lista-tarjetas');
+    contenedorPrincipal.appendChild(contenedorTarjetas);
+  }
+  
+  
+  
+  //EJECUTO LA FUNCION crearTarjetas CON LA DATA OBTENIDA CON EL GET A LA API.
+  $.get('https://students-api.2.us-1.fl0.io/movies', data => crearTarjetas(data));
+  
+  
+  
